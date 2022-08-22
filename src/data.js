a17adab691ebd4464faeb69604f7052cc37642bd
hagerElroby <hagerelroby.az2020@gmail.com>
2022-08-22 07:46:00 +0200

    My portfolio

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+
+export const products = [
+      {
+        id: 1,
+        img: require('./img/2.jpg'),
+        link: "https://hagerelroby.github.io/nokoSocial-/",
+      },
+      {
+        id: 2,
+        img: require('./img/1.jpg'),
+        link: "https://hagerelroby.github.io/Agency_Template-/",
+      },
+      {
+        id: 3,
+        img: require('./img/3.jpg'),
+        link: "https://recipe-web-app.vercel.app/",
+      },
+      {
+        id: 4,
+        img:  require('./img/4.jpg'),
+        link: "https://react18-gym-responsive-app.vercel.app/",
+      },
+      {
+        id: 5,
+        img: "./screencapture-hagerelroby-github-io-Agency-Template-2022-07-17-11_14_59.png",
+        link: "http://lama.dev",
+      },
+      {
+        id: 6,
+        img: "https://drive.google.com/file/d/1_3oo1iXZcgK9SgSenvrxLJ8cZS_IvYQE/view?usp=sharing",
+        link: "http://lama.dev",
+      },
+    ];
+    
+    
